7a67fd2e01a4d32291b65a37c1db14268321c013
Vu Doan <victordoan1708@gmail.com>
2020-04-17 21:30:17 +0100

    Displaying Profiles (On Progress). Issue with not being able to load all images from the database

--- public/profiles/p_sketch.js
+++ public/profiles/p_sketch.js
@@ -1,15 +1,47 @@
-let imgData;
+let imgData = []; // variable to store image data in JSON
+let images = []; // variable to create actual images from the image data
+
+let loading = true;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    displayProfiles();
+    retrieveData()
+        .then(data => {
+            imgData = data;
+        })
+        .then(() => {
+            // load all the images from the database
+            for (let j = 0; j < imgData.length; j++) {
+                images[j] = loadImage(imgData[j].image64);
+                console.log(j);
+            }
+            loading = false; // trigger displayProfiles() function   
+        })
+        .catch(err => {
+            console.log(err);
+        });
 }
 
 function draw() {
-    background(100);
+    background(41, 40, 40);
+    if (!loading) displayProfiles();
 }
 
-async function displayProfiles() {
+// display the profiles on webpage
+function displayProfiles() {
+    for (let j = 0 ; j < imgData.length; j++) {
+        for (let i = 0; i < imgData.length - 3; i++) {
+            // console.log(imgData.length);
+            let w = 320,
+                h = 240,
+                x = w * i,
+                y = h * j;
+            image(images[i], x, y, w, h);
+        }
+    }
+}
+
+async function retrieveData() {
     const old_data = await fetch('/face-api/');
     imgData = await old_data.json();
     console.log(imgData);
