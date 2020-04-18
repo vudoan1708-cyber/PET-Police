5b3c9ae4a790d9fc37806d7839f880ec64d570cd
Vu Doan <victordoan1708@gmail.com>
2020-04-19 00:31:29 +0100

    Display Profiles: Button and Text animations. Profile detail UNDONE

--- public/profiles/p_sketch.js
+++ public/profiles/p_sketch.js
@@ -11,9 +11,14 @@ let riskLevel = []; // variable to store total risk factors
 let loading = true;
 let loadingCounter = 0;
 
+// booleans
+let moveBackwards = false,
+    moveForwards =  false;
+
 // image size
 let x = 0,
-    w = 320;
+    w = 320,
+    h = 240;
 
 // animate image gallery
 let gallery_move = 1;
@@ -56,6 +61,7 @@ function setup() {
 
     rectMode(CENTER);
     imageMode(CENTER);
+    textAlign(CENTER);
 }
 
 function draw() {
@@ -64,6 +70,7 @@ function draw() {
     else background(0, 0, 255); // loading animation purpose (HAVEN'T DONE IT)
 }
 
+// responsive window resizing
 function windowResized() {
     resizeCanvas(window.innerWidth, window.innerHeight);
 }
@@ -74,22 +81,20 @@ const arrow_w = 50,
 // display the profiles on webpage
 function displayProfiles() {
     // images related parameters
-    let h = 240,
-        y = height / 2;
+    let y = height / 2;
 
     // titles and its background
     push();
         fill(0);
         rect(width / 2, height / 4, width, height / 5);
         fill(0, 20, 255, 150);
-        textAlign(CENTER);
         textSize(width / 40); // responsive text
         stroke(100, 100);
         strokeWeight(2);
         text("PROFILE VISUALISATIONS", width / 2, height / 4);
     pop();
 
-    // back arrow
+    // back arrow to the landing page
     push();
         translate(width / 15, height / 4);
         // hover the back button
@@ -100,6 +105,18 @@ function displayProfiles() {
                 noStroke();
                 rect(0, 0, arrow_w + 20, arrow_h + 20);
                 scale(1.5, 1.5);
+                push(); // to avoid the scale function for this rectangle
+                    strokeWeight(2.5);
+                    stroke(255, 0, 0, 100);
+                    noFill();
+                    rect(width / 15, -50, width / 8, 50);
+                    noStroke();
+                    fill(200);
+                    rect(width / 15, -50, width / 8, 50);
+                pop();
+                fill(10);
+                textSize(width / 100);
+                text("Back to the landing page", width / 15, -50);
             }
         }
         image(left_arrow, 0, 0, arrow_w, arrow_h);
@@ -107,7 +124,10 @@ function displayProfiles() {
 
 
     // image gallery
-    if (gallery_move > 1) {
+    if ( gallery_move > 1 && gallery_move < (storedData.length) * 2 - ((width / (2 * w) * 2)) ) { 
+    // steps needed < number of steps to get through all the images stored in the database - number of steps to display images to half of the screen
+    // because that is how it is by default (half a screen is full with images)
+    // each incremental step = half an image size
         for (let i = 0; i < images.length; i++) {
             x = ((w * i) + width / 2) - (w * gallery_move / 2);
             // if (mouseX > (-x / 2) && mouseX < (x / 2)) {
@@ -119,6 +139,7 @@ function displayProfiles() {
             image(images[i], x, y, w, h);
         }
     } else {
+        gallery_move = 1;
         for (let i = 0; i < images.length; i++) {
             x = (w * i) + width / 2;
             // if (mouseX > (-x / 2) && mouseX < (x / 2)) {
@@ -130,6 +151,18 @@ function displayProfiles() {
             image(images[i], x, y, w, h);
         }
     }
+    
+    // image gallery control
+    if (moveBackwards) {
+        moveBackwards = false;
+        gallery_move--;
+        console.log(gallery_move)
+    } else if (moveForwards) {
+        moveForwards = false;
+        gallery_move++;
+        console.log(gallery_move)
+    }
+
     // draw some horizontal lines for decorations
     push();
         stroke(255);
@@ -142,6 +175,43 @@ function displayProfiles() {
         line(0, height / 2 - h / 2, width, height / 2 - h / 2);
         line(0, height / 2 + h / 2, width, height / 2 + h / 2);
     pop();
+
+    // draw forward and backward arrows for image gallery
+    push();
+        push();
+            // backward button
+            // hide it if it gets less than 1, so users stop pressing it
+            if (gallery_move > 1) {
+                translate(width / 2 - 100, height / 2 + h);
+                // hover backward button
+                if (mouseX > width / 2 - 150 && mouseX < width / 2 - 45) {
+                    if (mouseY > height / 2 + h - 35 && mouseY < height / 2 + h + 45) {
+                        scale(1.25, 1.25);
+                    }
+                } 
+                fill(0, 100);
+                triangle(-50, 5, 55, 45, 55, -35);
+                fill(200);
+                triangle(-50, 0, 50, 40, 50, -40);
+            }
+        pop();   
+
+        push();
+            // forward button
+            translate(width / 2 + 100, height / 2 + h);
+            if (mouseX < width / 2 + 150 && mouseX > width / 2 + 45) {
+                if (mouseY > height / 2 + h - 35 && mouseY < height / 2 + h + 45) {
+                    scale(1.25, 1.25);
+                }
+            }
+            fill(0, 100);
+            triangle(50, 5, -55, 45, -55, -35);
+            fill(200);
+            triangle(50, 0, -50, 40, -50, -40);
+        pop();
+    pop();
+
+    
 }
 
 // back to the landing page
@@ -149,22 +219,36 @@ function goBack() {
     location.href = "../index.html";
 }
 
+
 function mousePressed() {
+    // go back to the landing page
     if (mouseX > (width / 15 - arrow_w / 2) && mouseX < (width / 15 + arrow_w / 2)) {
         if (mouseY > (height / 4 - arrow_h / 2) && mouseY < (height / 4 + arrow_h / 2)) {
             goBack();
         }
     }
+
+    // backward button for image gallery
+    if (mouseX > width / 2 - 150 && mouseX < width / 2 - 45) {
+        if (mouseY > height / 2 + h - 35 && mouseY < height / 2 + h + 45) {
+            moveBackwards = true;
+        }
+    }
+
+    // forward button for image gallery
+    if (mouseX < width / 2 + 150 && mouseX > width / 2 + 45) {
+        if (mouseY > height / 2 + h - 35 && mouseY < height / 2 + h + 45) {
+            moveForwards = true;
+        }
+    }
 }
 
-// gallery control
+// for testing
 function keyPressed() {
     if (keyCode === RIGHT_ARROW) {
         gallery_move++;
-        console.log(gallery_move);
     } else if (keyCode === LEFT_ARROW) {
         gallery_move--;
-        console.log(gallery_move);
     }
 }
 
