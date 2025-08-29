6b3665abd61f243b54c10ebee0eb4b95f9c1c30b
Vu Doan <vu@voly.co.uk>
2025-08-29 01:03:10 +0100

    connect to mongodb and replace nedb

--- server.js
+++ server.js
@@ -1,25 +1,19 @@
+require('dotenv').config();
+
 const express = require("express"); // load express
-const Datastore = require("nedb"); // load nedb database
+const createConnection = require('./data/connection');
+
 const port = process.env.PORT || 5000;
 const app = express(); // setup express
 const http = require("http");
 const server = http.createServer(app);
 
-const { check, validationResult } = require('express-validator');
-
-const database = new Datastore("database/database.db"); // set up database object and set up the directory to where it will be saved
-// const user_database = new Datastore("database/user_database");
-database.loadDatabase(); // create a database file
-// user_database.loadDatabase();
-// const path = require('path'); // path to the logo 
-
 server.listen(port, () => {
   console.log(`Listening on port: ${port}`);
 });
-app.use(express.static("public")); // setup root directory for client side
-app.use(express.json({ limit: "100mb" })) // set limit for data transfer
-// app.use('/images', express.static(path.join('assets/police_logo.svg', 'images')))
+app.use(express.json({ limit: "100mb" }))
 
+const detectionResults_db = createConnection('detectionResults');
 
 // save images to server
 app.post('/face-api/', (request, response) => {
@@ -34,27 +28,19 @@ app.post('/face-api/', (request, response) => {
     // console.log(getDate);
     data.timestamp = getDate;
   
-    let base64String = data.image64; // image data in Base64 format 
-    let base64Image = base64String.split(';base64,').pop(); // remove header
-    // write to file
-    require("fs").writeFile(`database/img/image${data.totalImg}.png`, base64Image, {encoding: 'base64'}, function(err) {
-      console.log('File created');
-    });
-  
-    database.insert(data);
+    detectionResults_db.insert(data);
     response.json(data);
   });
 
-app.get('/face-api/', (request, response) => {
-  // find all data from database, sort them by totalImg and execute 2 functions
-  database.find({}).sort({ totalImg: -1 }).exec((err, data) => { 
-    // console.log(docs);
-    if (err) {
-      console.error(err);
-      response.end();
-      return;
-    } else response.json(data);
-  })
+app.get('/face-api/', async (request, response) => {
+  try {
+    const data = await detectionResults_db.find({}, { sort: { totalImg: -1 } });
+    response.json(data);
+  } catch (err) {
+    console.error(err);
+    response.end();
+    throw (err);
+  }
 });
 
 // forms
@@ -81,3 +67,4 @@ app.get('/face-api/', (request, response) => {
 //     } else response.json(data);
 //   })
 // });
+app.use(express.static("public"));
